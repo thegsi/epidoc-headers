c789e6d355148333be1540fdce0035670bef5810
Gethin Rees <idp1@idp.ad.bl.uk>
2017-09-05 16:44:34 +0100

    add Daniel corrections, remove extra BOM from rootSnippets

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # epidoc-headers
 Create epidoc headers for Siddham metadata using python
+
+Run `python encloseSnippets.py` first to add sanskrit edition div.
+
+Next `createHeader.py` to create XML header from metadata csv.
+
+Remember: Metadata csv is formatted so that objects and inscriptions are columns not rows.
+
+Requires snippets, rootSnippets, metadata (object.csv, inscription.csv), headed and declared directories.
--- encloseSnippets.py
+++ encloseSnippets.py
@@ -1,5 +1,6 @@
 import itertools
 import pandas as pd
+import codecs
 import xml.etree.ElementTree as ET
 ET.register_namespace('', 'http://www.tei-c.org/ns/1.0')
 
@@ -13,5 +14,11 @@ for inscriptionId in inscriptionIds:
     print(inscriptionId)
     snippetFileName = './snippets/' + inscriptionId + '.xml'
     rootSnippetFileName = './rootSnippets/' + inscriptionId + '.xml'
-    with open(snippetFileName, 'rb') as f, open(rootSnippetFileName, 'wb') as g:
-        g.write('<div type="edition" xml:lang="san-Latn">{}</div>'.format(f.read()))
+    with codecs.open(snippetFileName, 'rb') as f, codecs.open(rootSnippetFileName, 'w') as g:
+        fileRead = f.read()
+        # Remove BOM (zero-width non-breaking space) from start of snippetFile
+        # https://stackoverflow.com/questions/8898294/convert-utf-8-with-bom-to-utf-8-with-no-bom-in-python#8898439
+        fileReadDecode = fileRead.decode('utf-8-sig')
+        fileReadEncode = fileReadDecode.encode('utf-8')
+        output = '<div type="edition" xml:lang="san-Latn">\n{0}</div>'.format(fileReadEncode)
+        g.write(output)
